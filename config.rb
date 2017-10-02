b3a04fba03dae60858ed9ac42dca6333f606e0c2
Ashley Whitcher <amerritt@theadvocate.com>
2017-10-02 16:28:45 -0500

    Simplify code

--- config.rb
+++
@@ -1,9 +0,0 @@
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
-set :relative_links, true
-
-configure :build do
-  activate :minify_css
-  activate :minify_javascript
-end
